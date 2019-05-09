4e4af4359469ac1335db926e9487e073772c9442
Brodie Brock <brodiebrock@Brodies-MacBook-Pro.local>
2019-05-09 12:14:48 -0400

    working calculator

--- js/script.js
+++ js/script.js
@@ -1,43 +1,100 @@
 "use strict";
 
 const input = document.getElementById('input'), // input/output button
-
     number = document.querySelectorAll('.numbers div'), // number buttons
     operator = document.querySelectorAll('.operators div'), // operator buttons
     result = document.getElementById('result'), // equal button
     clear = document.getElementById('clear'); // clear button
     
-let resultDisplayed = true; // flag to keep an eye on what output is displayed
+let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // adding click handlers to number buttons
-number.forEach(function(num){
-    num.addEventListener('click', function() {
-        input.innerHTML += this.innerHTML;
-        console.log(this.innerHTML);
+Array.from(number).map(number => {
+    number.addEventListener("click", function() {
+
+        let currentString = input.innerHTML;
+        let lastCharacter = currentString[currentString.length - 1];
+
+        if (resultDisplayed === false){
+            input.innerHTML += this.innerHTML;
+        } else if (
+            (resultDisplayed === true && lastCharacter === "+") ||
+            (resultDisplayed === true && lastCharacter === "-") ||
+            (resultDisplayed === true && lastCharacter === "*") ||
+            (resultDisplayed === true && lastCharacter === "/")
+    )
+
+            {
+
+            resultDisplayed = false;
+            input.innerHTML += this.innerHTML;
+        } else {
+
+            resultDisplayed= false;
+            input.innerHTML = "";
+            input.innerHTML += this.innerHTML;
+
+        }
     });
 });
+
 
 // adding click handlers to the operation buttons
 operator.forEach(function(operationButton){
     operationButton.addEventListener('click', function() {
-        let currentString = input.innerHTML;
         let lastCharacter = input.innerHTML[input.innerHTML.length -1];
-        if (lastCharacter === "+" || lastCharacter === "-" || lastCharacter === "×" || lastCharacter === "÷") {
+        if (lastCharacter === "+" || lastCharacter === "-" || lastCharacter === "*" || lastCharacter === "/") {
             var newString = currentString.substring(0, currentString.length - 1) + this.innerHTML;
             input.innerHTML = newString;
-        } else if (currentString.length == 0) {
+        } else if (input.innerHTML.length == 0) {
             console.log("enter a number first");
         } else {
             input.innerHTML += this.innerHTML;
-        }
-    })
+        };
+    });
 });
 
 // on click of 'equal' button
 result.addEventListener('click', function() {
-    let inputString = input.innerHTML;
-    const numbers = inputString.split(/\+|\-|\×|\÷/g);
+    const currentString = input.innerHTML;
+    const numberStringArray = currentString.split(/\+|\-|\*|\//g);
 
+    let numbersArray = []; 
+
+    numberStringArray.forEach(function(number){
+        
+        numbersArray.push(Number(number));
+    });
+    const operatorsArray = currentString.replace(/[0-9]|\./g,"").split("");
+    
+    //four while loops for each math operation
+    let multiply = operatorsArray.indexOf("*");
+    while (multiply != -1) {
+        numbersArray.splice(multiply, 2, numbersArray[multiply] * numbersArray[multiply +1])
+        operatorsArray.splice(multiply, 1);
+        multiply = operatorsArray.indexOf("*");
+    }
+
+    let divide = operatorsArray.indexOf("/");
+    while (divide != -1) {
+        numbersArray.splice(divide, 2, numbersArray[divide] / numbersArray[divide + 1])
+        operatorsArray.splice(divide, 1);
+        divide = operatorsArray.indexOf("/");
+    }
+    let add = operatorsArray.indexOf("+");
+    while (add != -1) {
+        numbersArray.splice(add, 2, numbersArray[add] + numbersArray[add + 1])
+        operatorsArray.splice(add, 1);
+        add = operatorsArray.indexOf("+");
+    }
+    let subtract = operatorsArray.indexOf("-")
+    while (subtract != -1) {
+        numbersArray.splice(subtract, 2, numbersArray[subtract] + numbersArray[subtract +1])
+        operatorsArray.splice(subtract, 1);
+        subtract = operatorsArray.indexOf("-");
+    }
+
+    input.innerHTML = numbersArray;
 });
 
 
